b076bbad3c68702a044e53dcb8e89dc7712d326d
Richard Awoyemi <Richardawoyemi@gmail.com>
2022-07-17 00:44:10 +0100

    Fixed netlify function

--- functions/getTradesByContract.js
+++ functions/getTradesByContract.js
@@ -5,19 +5,19 @@ const headers = {
   'X-API-Key': '38d74028-ca13-48df-ab81-bdfa4f3ab834',
 };
 
-// const API_ENDPOINT =
-// 'https://api.x2y2.org/v1/events?type=sale&from_address&to_address=0x2ef1630993bc569a18f8c406ab720e2d040e155a&contract';
-
 export async function handler(event) {
-  const from = event.queryStringParameters.from
-    ? '&from_address=' + event.queryStringParameters.from
-    : '';
-  const to = event.queryStringParameters.from
-    ? '&to_address=' + event.queryStringParameters.to
-    : '';
-  const contract = event.queryStringParameters.from
-    ? '&contract=' + event.queryStringParameters.contract
-    : '';
+  const from =
+    event.queryStringParameters.from != undefined
+      ? '&from_address=' + event.queryStringParameters.from
+      : '';
+  const to =
+    event.queryStringParameters.to != undefined
+      ? '&to_address=' + event.queryStringParameters.to
+      : '';
+  const contract =
+    event.queryStringParameters.contract != undefined
+      ? '&contract=' + event.queryStringParameters.contract
+      : '';
   const API_ENDPOINT =
     'https://api.x2y2.org/v1/events?type=sale' + from + to + contract;
 
