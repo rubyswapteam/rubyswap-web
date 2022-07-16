5efab36a77a3006b04830e4b1a44c83147719557
Richard Awoyemi <Richardawoyemi@gmail.com>
2022-07-16 23:45:54 +0100

    Function fixed

--- functions/dist/getData.dev.js
+++ functions/dist/getData.dev.js
@@ -9,39 +9,44 @@ var _nodeFetch = _interopRequireDefault(require("node-fetch"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// const fetch = require('node-fetch');
 var headers = {
   'content-type': 'application/json',
-  // 'Access-Control-Allow-Origin': '*',
-  'X-API-KEY': '38d74028-ca13-48df-ab81-bdfa4f3ab834'
-};
-var API_ENDPOINT = 'https://api.x2y2.org/v1/events?type=sale&from_address&to_address=0x2ef1630993bc569a18f8c406ab720e2d040e155a&contract';
+  'X-API-Key': '38d74028-ca13-48df-ab81-bdfa4f3ab834'
+}; // const API_ENDPOINT =
+// 'https://api.x2y2.org/v1/events?type=sale&from_address&to_address=0x2ef1630993bc569a18f8c406ab720e2d040e155a&contract';
 
-function handler() {
-  var response, data;
+function handler(event) {
+  var from, to, contract, API_ENDPOINT, response, data;
   return regeneratorRuntime.async(function handler$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          _context.prev = 0;
-          _context.next = 3;
-          return regeneratorRuntime.awrap((0, _nodeFetch["default"])(API_ENDPOINT, headers));
+          from = event.queryStringParameters.from ? '&from_address=' + event.queryStringParameters.from : '';
+          to = event.queryStringParameters.from ? '&to_address=' + event.queryStringParameters.to : '';
+          contract = event.queryStringParameters.from ? '&contract=' + event.queryStringParameters.contract : '';
+          API_ENDPOINT = 'https://api.x2y2.org/v1/events?type=sale' + from + to + contract;
+          console.log(API_ENDPOINT);
+          _context.prev = 5;
+          _context.next = 8;
+          return regeneratorRuntime.awrap((0, _nodeFetch["default"])(API_ENDPOINT, {
+            headers: headers
+          }));
 
-        case 3:
+        case 8:
           response = _context.sent;
-          _context.next = 6;
+          _context.next = 11;
           return regeneratorRuntime.awrap(response.json());
 
-        case 6:
+        case 11:
           data = _context.sent;
           return _context.abrupt("return", {
             statusCode: 200,
             body: JSON.stringify(data)
           });
 
-        case 10:
-          _context.prev = 10;
-          _context.t0 = _context["catch"](0);
+        case 15:
+          _context.prev = 15;
+          _context.t0 = _context["catch"](5);
           console.log(_context.t0);
           return _context.abrupt("return", {
             statusCode: 500,
@@ -50,10 +55,10 @@ function handler() {
             })
           });
 
-        case 14:
+        case 19:
         case "end":
           return _context.stop();
       }
     }
-  }, null, null, [[0, 10]]);
+  }, null, null, [[5, 15]]);
 }
--- functions/getData.js
+++ functions/getData.js
@@ -1,19 +1,29 @@
 import fetch from 'node-fetch';
 
-// const fetch = require('node-fetch');
-
 const headers = {
   'content-type': 'application/json',
-  // 'Access-Control-Allow-Origin': '*',
-  'X-API-KEY': '38d74028-ca13-48df-ab81-bdfa4f3ab834',
+  'X-API-Key': '38d74028-ca13-48df-ab81-bdfa4f3ab834',
 };
 
-const API_ENDPOINT =
-  'https://api.x2y2.org/v1/events?type=sale&from_address&to_address=0x2ef1630993bc569a18f8c406ab720e2d040e155a&contract';
+// const API_ENDPOINT =
+// 'https://api.x2y2.org/v1/events?type=sale&from_address&to_address=0x2ef1630993bc569a18f8c406ab720e2d040e155a&contract';
 
-export async function handler() {
+export async function handler(event) {
+  const from = event.queryStringParameters.from
+    ? '&from_address=' + event.queryStringParameters.from
+    : '';
+  const to = event.queryStringParameters.from
+    ? '&to_address=' + event.queryStringParameters.to
+    : '';
+  const contract = event.queryStringParameters.from
+    ? '&contract=' + event.queryStringParameters.contract
+    : '';
+  const API_ENDPOINT =
+    'https://api.x2y2.org/v1/events?type=sale' + from + to + contract;
+
+  console.log(API_ENDPOINT);
   try {
-    const response = await fetch(API_ENDPOINT, headers);
+    const response = await fetch(API_ENDPOINT, { headers: headers });
     const data = await response.json();
     return { statusCode: 200, body: JSON.stringify(data) };
   } catch (error) {
